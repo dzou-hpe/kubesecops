30fd807ef0d8024e39cd0d6fddb6e764f34ac8de
Ahoy <ahoy@algol60.net>
2022-06-12 22:13:57 -0400

    revert debug changes

--- hack/deployCRD.sh
+++ hack/deployCRD.sh
@@ -9,7 +9,7 @@ else
 fi
 
 kubectl delete -f examples/zap/crd.yaml || true
-#kubectl delete -f https://gist.githubusercontent.com/sdenel/1bd2c8b5975393ababbcff9b57784e82/raw/f1b885349ba17cb2a81ca3899acc86c6ad150eb1/nginx-hello-world-deployment.yaml || true
+kubectl delete -f https://gist.githubusercontent.com/sdenel/1bd2c8b5975393ababbcff9b57784e82/raw/f1b885349ba17cb2a81ca3899acc86c6ad150eb1/nginx-hello-world-deployment.yaml || true
 
 kubectl apply -f https://gist.githubusercontent.com/sdenel/1bd2c8b5975393ababbcff9b57784e82/raw/f1b885349ba17cb2a81ca3899acc86c6ad150eb1/nginx-hello-world-deployment.yaml
 kubectl apply -f examples/zap/crd.yaml
